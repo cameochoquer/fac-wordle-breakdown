9f1d9b5ccc4bbc075e9beffe681eaeff58c2a2a4
cameo choquer <cjchoquer@gmail.com>
2023-03-17 10:39:30 +0000

    added basic HTML structure and test

--- index.test.js
+++ index.test.js
@@ -1,14 +1,19 @@
 function equal(actual, expected, message) {
-    if (actual === expected) {
-      const defaultMessage = `Expected ${expected} and received ${actual}`;
-      console.info("Pass: " + (message || defaultMessage));
-    } else {
-      const defaultMessage = `Expected ${expected} but received ${actual} instead`;
-      console.error("Fail: " + (message || defaultMessage));
-    }
+  if (actual === expected) {
+    const defaultMessage = `Expected ${expected} and received ${actual}`;
+    console.info("Pass: " + (message || defaultMessage));
+  } else {
+    const defaultMessage = `Expected ${expected} but received ${actual} instead`;
+    console.error("Fail: " + (message || defaultMessage));
   }
-  function test(name, testFunction) {
-    console.group(name);
-    testFunction();
-    console.groupEnd(name);
-  }
+}
+function test(name, testFunction) {
+  console.group(name);
+  testFunction();
+  console.groupEnd(name);
+}
+
+test("checks word has only letters", () => {
+  const result = validateWord("hello");
+  equal(result, false);
+});
